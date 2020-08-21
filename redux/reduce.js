508336b156f1ffa7af391847e810b3397e7f4a6b
暖生 <826237710@qq.com>
2020-08-21 12:54:21 +0800

    Update reduce.js

--- redux/reduce.js
+++ redux/reduce.js
@@ -1,6 +1,7 @@
 module.exports.compose = (middlewares) => {
     if (middlewares.length === 0) {
-        return () => {}
+        return (arg) => arg;
     }
-    return (next =async () => {}) => middlewares.reduce((a, b) => arg => a(() => b(arg)))(next)
-}
+    return (next = async () => {}) =>
+        middlewares.reduce((a, b) => (...arg) => a(b(...arg)))(next);
+};
