21b7c70adc01a990a10ba6c5385383778c32a40b
helloyellow <helloyellow@aliyun.com>
2020-08-23 17:43:10 +0800

    添加reduceRight实现方式执行过程说明

--- redux/reduceRight.js
+++ redux/reduceRight.js
@@ -1,7 +1,17 @@
+/**
+ * @description: 利用reduceRight实现洋葱圈
+ * @param {middlewares：中间件数组} 
+ * @return {高阶函数} 
+ */
 module.exports.compose = (middlewares) => {
     if (!Array.isArray(middlewares)) {
         middlewares = Array.from(arguments);
     }
+    // reduceRight：从右向左做累加，第二个参数为初始值
+    // 比如：middlewares = [f1, f2, f3]，执行过程如下：
+    // 第一次 --> a = () => {}，                    b = f3，结果：() => f3(() => {})
+    // 第二次 --> a = () => f3(() => {})，          b = f2，结果：() => f2(() => f3(() => {}))
+    // 第三次 --> a = () => f2(() => f3(() => {}))，b = f1，结果：() => f1(() => f2(() => f3(() => {})))
     return () =>
         middlewares.reduceRight(
             (a, b) => () => b(a),
