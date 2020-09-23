71fccb14636d27bd0e4ac01f39c9ca7aa10df55e
lixx3000 <45725941+lixx3000@users.noreply.github.com>
2020-09-23 08:35:24 +0800

    Update index.js

--- generator/index.js
+++ generator/index.js
@@ -8,11 +8,12 @@ module.exports.compose = middlewares => {
       }
     }
     const chainCenter = chain();
+    const start = chainCenter.next().value;
     // 这里的proxy配合generator模拟递归执行
     function proxy () {
       const fn = chainCenter.next().value;
-      const value = fn && fn(proxy);
+      const value = typeof fn === 'function' && fn && fn(proxy);
       return Promise.resolve(value);
     }
-    return Promise.resolve(chainCenter.next().value(proxy))
+    return Promise.resolve(typeof start === 'function' && start(proxy))
 }
