4e12235c52c4d948a50ce57b00c128279ce2eb49
lixx3000 <45725941+lixx3000@users.noreply.github.com>
2020-09-23 09:27:12 +0800

    函数在包一层函数

--- generator/index.js
+++ generator/index.js
@@ -1,7 +1,7 @@
 // 第21期学员李宇轩
 // 使用generator实现
 // 高端的代码，往往只需要最朴素的设计方法，忙碌了一天的小李，偶然发现了责任链模式，从此展开了新的人生旅途
-module.exports.compose = middlewares => {
+module.exports.compose = middlewares => () => {
     function* chain() {
       for (let index = 0,length = middlewares.length;index < length; ++index) {
         yield middlewares[index]
