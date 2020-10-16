45f333574b8518917f44b2550f576b52f064f50a
josephxia <15906475@qq.com>
2020-10-16 17:14:41 +0800

    update

--- template/__tests__/index.spec.js
+++ template/__tests__/index.spec.js
@@ -5,6 +5,16 @@ describe("模板编译", () => {
     expect(output).toBe(`<b>tom</b>`);
   });
 
+  it("{{}} toUpperCase 表达式", () => {
+    const output = compile("<b>{{ name.toUpperCase() }}</b>")({ name: "tom" });
+    expect(output).toBe(`<b>TOM</b>`);
+  });
+
+  it("{{}} +连接", () => {
+    const output = compile("<b>{{ '[' + name + ']' }}</b>")({ name: "tom" });
+    expect(output).toBe(`<b>[tom]</b>`);
+  });
+
   it("forEach 遍历", () => {
     const output = compile(
 `{%arr.forEach(item => {%}
