5c48424b5bdeb1247da9ebb0999394aa383c3e7a
xinxinshen <xinxin.shen@beibei.com>
2020-10-19 18:02:32 +0800

    throttle_debounce

--- throttle_debounce/index.js
+++ throttle_debounce/index.js
@@ -1,32 +1,55 @@
 /**
  * 节流Throttle
  */
-module.exports.throttle = (fn, delay) => {
-  // 定义上次触发时间
-  let last = 0;
-  return (...args) => {
-    const now = + Date.now();
-    console.log("call", now, last, delay);
-    if (now > last + delay) {
-      last = now;
-      fn.apply(this, args);
-    }
-  };
-};
+// module.exports.throttle = (fn, delay) => {
+//   // 定义上次触发时间
+//   let last = 0;
+//   return (...args) => {
+//     const now = + Date.now();
+//     console.log("call", now, last, delay);
+//     if (now > last + delay) {
+//       last = now;
+//       fn.apply(this, args);
+//     }
+//   };
+// };
 /**
  * 防抖Debounce
  */
+// module.exports.debounce = (fn, delay) => {
+//   let timer;
+//   return (...args) => {
+//     // 判断定时器是否存在，清除定时器
+//     if (timer) {
+//       clearTimeout(timer);
+//     }
+
+//     // 重新调用setTimeout
+//     timer = setTimeout(() => {
+//       fn.apply(this, args);
+//     }, delay);
+//   };
+// };
+
+module.exports.throttle = (fn, delay)=>{
+  let last = 0;
+  return (...args)=>{
+    const now = Date.now();
+    if(now > last + delay) {
+      last = now;
+      fn.apply(this, args)
+    }
+  }
+}
+
 module.exports.debounce = (fn, delay) => {
   let timer;
   return (...args) => {
-    // 判断定时器是否存在，清除定时器
-    if (timer) {
+    if(timer) {
       clearTimeout(timer);
     }
-
-    // 重新调用setTimeout
-    timer = setTimeout(() => {
+    timer = setTimeout(()=>{
       fn.apply(this, args);
     }, delay);
-  };
-};
+  }
+}
