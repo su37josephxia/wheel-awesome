ba0a105232eb3e5eca46069a88d416b6ddb0769a
fuzhaoyang <932647051@qq.com>
2022-02-17 10:37:17 +0800

    fix: 1、增加mini-vue diff 测试用例 2、删除vdom diff未引用import修复测试用例

--- 03-wheel/mini-vue/runtime/diff/__tests__/index.spec.js
+++ 03-wheel/mini-vue/runtime/diff/__tests__/index.spec.js
@@ -1,8 +1,30 @@
-// exports.diffArray = (n1,n2, {
-//     mountElement,
-//     remove
-// }) => { 
-//     mountElement(1)
-
-//     remove(5)
-// }
+describe("数组Diff", () => {
+    it("增加元素", () => {
+      const mountElement = jest.fn();
+      const remove = jest.fn();
+      const { diffArray } = require("../index");
+      diffArray([1, 2, 3, 5], [1, 2, 3, 1], { mountElement, remove });
+  
+      // 第一次调用次数
+      expect(mountElement.mock.calls.length).toBe(1);
+      // 第一次调用的第一个参数
+      expect(mountElement.mock.calls[0][0]).toBe(1);
+  
+      expect(remove.mock.calls.length).toBe(1);
+      expect(remove.mock.calls[0][0]).toBe(5);
+    });
+  
+    it("删除元素", () => {
+      const mountElement = jest.fn();
+      const remove = jest.fn();
+      const { diffArray } = require("../index");
+      diffArray([1, 2, 3, 5], [1, 2, 3, 1], { mountElement, remove });
+  
+      expect(mountElement.mock.calls.length).toBe(1);
+      expect(mountElement.mock.calls[0][0]).toBe(1);
+  
+      expect(remove.mock.calls.length).toBe(1);
+      expect(remove.mock.calls[0][0]).toBe(5);
+    });
+  });
+  
