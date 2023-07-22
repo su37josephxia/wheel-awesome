3107ac95351491f6b5dc1bd5386797d4eeb9cbbb
josephxia <15906475@qq.com>
2023-07-22 12:50:52 +0800

    feat: 添加动态规划解法

--- 04-algrithm/01-table/index.js
+++ 04-algrithm/01-table/index.js
@@ -23,3 +23,25 @@ exports.checkMem = function checkMem(remain, pre, max = 10) {
     }
     return memo[remain + '-' + pre] = cnt;
 }
+
+exports.checkDP = function checkDP(N, pre, M = 10) {
+    var table = new Array(M + 1)
+    for (var i = 0; i <= M; i++) {
+        table[i] = new Array(N + 1)
+        for (var j = 0; j <= N; j++) {
+            table[i][j] = 0;
+        }
+    }
+
+    for (var i = 0; i <= M; i++) {
+        table[i][0] = 1;
+    }
+
+    for (var i = 1; i <= M; i++) {
+        for (var j = 2; j <= N; j++) {
+            if ((i >= 2) && (j >= i)) table[i][j] += table[i][j - i];
+            if (i > 2) table[i][j] += table[i - 1][j];
+        }
+    }
+    return table[M][N];
+}
