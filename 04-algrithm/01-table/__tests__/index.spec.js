e03e580ecbc2c9f9a075bec8c82a0a9b9defbf4c
josephxia <15906475@qq.com>
2023-07-22 12:29:05 +0800

    feat: 添加分桌子算法题

---
+++ 04-algrithm/01-table/__tests__/index.spec.js
@@ -0,0 +1,26 @@
+const { check, checkMem } = require('../index');
+describe('分桌坐 ', () => {
+    describe('递归解法', () => {
+        it('6人 每桌最少坐2人 4种分法', () => {
+            expect(check(6, 2, 10)).toBe(4);
+        })
+        it('10人 每桌最少坐2人 12种分法', () => {
+            expect(check(10, 2, 10)).toBe(12);
+        })
+        it('100人 每桌最少坐2人 437420种分法', () => {
+            expect(check(100, 2, 10)).toBe(437420);
+        })
+    })
+
+    describe('递归解法 + 内存化', () => {
+        it('6人 每桌最少坐2人 4种分法', () => {
+            expect(checkMem(6, 2, 10)).toBe(4);
+        })
+        it('10人 每桌最少坐2人 12种分法', () => {
+            expect(checkMem(10, 2, 10)).toBe(12);
+        })
+        it('100人 每桌最少坐2人 437420种分法', () => {
+            expect(checkMem(100, 2, 10)).toBe(437420);
+        })
+    })
+})           
