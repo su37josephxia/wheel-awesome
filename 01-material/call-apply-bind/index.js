575340d235356badf01c678e41e43fca2529119d
rayadaschn <115447518+rayadaschn@users.noreply.github.com>
2023-09-13 22:39:10 +0800

    fix: 完善 call/apply 类型检测修复

--- 01-material/call-apply-bind/index.js
+++ 01-material/call-apply-bind/index.js
@@ -1,19 +1,22 @@
 exports.call = function (context, ...args) {
   // this 为调用方法 例:f.call this = f
-  context.fn = this;
-  const result = context.fn(...args);
-  delete context.fn;
+  if (context == null) context = globalThis;
+  if (typeof context !== "object") context = new Object(context);
+
+  const fnKey = Symbol();
+  context[fnKey] = this;
+  const result = context[fnKey](...args);
+  delete context[fnKey];
   return result;
 };
 
 exports.apply = function (context, args) {
-
-    return this.call(context,...args)
-//   // this 为调用方法 例:f.call this = f
-//   context.fn = this;
-//   const result = context.fn(...args);
-//   delete context.fn;
-//   return result;
+  return this.call(context, ...args);
+  //   // this 为调用方法 例:f.call this = f
+  // context.fn = this;
+  // const result = context.fn(...args);
+  // delete context.fn;
+  // return result;
 };
 
 exports.bind = function (context, ...args) {
